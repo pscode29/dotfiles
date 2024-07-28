2e0bb718d49e086fff7143976549be1c4740b816
Pushpraj Singh <thepushpraj@live.com>
2024-07-28 21:27:29 +0100

    add tmux plugin manager

---
+++ README.md
@@ -0,0 +1,22 @@
+# Install and stow
+
+```
+./install.sh
+```
+
+# Install Mason plugins
+
+- Run `:MasonInstallAll` in Neovim
+
+# Install Tmux plugins
+
+```
+cd ~
+# Run tmux
+tmux
+# Source tmux conf
+tmux source-file .tmux.conf
+# Install plugins
+<C-a> I
+```
+
--- install.sh
+++ install.sh
@@ -9,12 +9,16 @@ mv .tmux.conf .tmux.conf_backup
 mv .zshrc .zshrc_backup
 rm -rf ~/.local/state/nvim
 rm -rf ~/.local/share/nvim
+rm -rf .tmux
 
 mkdir .config
 
 # Brew install
 brew install neovim starship tmux stow ripgrep
 brew install --cask alacritty
+
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # stow everything
 cd dotfiles
