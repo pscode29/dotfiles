25dabf79d59fb7aa652d8e81f4a11413b59a08b2
Pushpraj Singh <thepushpraj@live.com>
2024-08-01 21:39:12 +0100

    make nvimtree width same as longest filename

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,16 +6,38 @@ return {
       require "configs.conform"
     end,
   },
+
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+
   {
     "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    -- See options by running
+    opts = {
+      filters = {
+        dotfiles = false,
+        -- show files even if they are git ignored
+        git_ignored = false,
+      },
+      view = {
+        side = "left",
+        -- This makes sure that longest file/folder width is assumed
+        -- which I like
+        width = {
+          max = -1,
+        },
+      },
+    },
   },
 
   {
