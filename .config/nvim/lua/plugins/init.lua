b52557023adbe4403827738bb78b16952b28b617
Pushpraj Singh <thepushpraj@live.com>
2024-08-05 00:20:37 +0100

    add telescope config for hidden files and symlinks

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,8 @@
+-- Add your plugins here or configure NvChad builtin plugins like nvim tree etc
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncommented for format on save
+    event = "BufWritePre", -- Uncommented for format on save
     config = function()
       require "configs.conform"
     end,
@@ -21,6 +22,55 @@ return {
   },
 
   {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        -- Find find hidden files and symlinks too on find_files ie <leader>ff
+        find_files = {
+          follow = true, -- Find symlinks
+          hidden = true, -- Find hidden files
+          file_ignore_patterns = { -- Dont find these files/dirs
+            "node_modules",
+            "build",
+            "dist",
+            "yarn.lock",
+            ".git",
+            ".idea",
+            ".vscode",
+            "package-lock.json",
+          },
+        },
+      },
+      defaults = {
+        vimgrep_arguments = {
+          -- This is for live grep using ripgrep i.e. <leader>fw
+          -- More here about ripgrep - https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file
+          -- For e.g. you can take this configuration out in ripgreprc file
+          "rg",
+          "--follow", -- Follow symbolic links
+          "--hidden", -- Search for hidden files
+          "--no-heading", -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number", -- Show line numbers
+          "--column", -- Show column numbers
+          -- Smart case search, other option is ignore-case,
+          -- but smart case is better because this is similar
+          -- to --ignore-case, but disables itself if the pattern contains any uppercase letters
+          "--smart-case",
+          -- Exclude some patterns from search
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+        },
+      },
+    },
+  },
+
+  {
     "nvim-tree/nvim-tree.lua",
     -- See options by running
     opts = {
@@ -32,10 +82,11 @@ return {
       view = {
         side = "left",
         -- This makes sure that longest file/folder width is assumed
-        -- which I like
-        width = {
-          max = -1,
-        },
+        -- which I like but at work I have some really long file names
+        -- width = {
+        --   max = -1,
+        -- },
+        width = 50,
       },
     },
   },
