90b46876d8489eb0dfdb0d7c92c2fd7cf86a2926
Pushpraj Singh <thepushpraj@live.com>
2025-01-11 13:00:04 +0000

    update ghostty config, add some keymaps

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,3 +10,13 @@ map("n", "<C-k>", "<cmd> TmuxNavigateUp<cr>", { desc = "Window up" })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<cr>", { desc = "Window down" })
 
 map("i", "jk", "<ESC>")
+
+-- Apply multiple identation without leaving v mode
+map("v", ">", ">gv", { noremap = true, silent = true })
+map("v", "<", "<gv", { noremap = true, silent = true })
+
+-- Send delete items to black hole register so does not come back on pressing p
+map("n", "d", '"_d', { noremap = true, silent = true })
+map("n", "c", '"_c', { noremap = true, silent = true })
+-- I like line delete to go to unnamed register so available on pressing p
+map("n", "dd", "dd", { noremap = true, silent = true })
